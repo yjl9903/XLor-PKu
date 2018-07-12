f21ce2ff3be0e6cb5f8ca97fd01b04a9b7e2bd3c
X1 Carbon <yjl9903@vip.qq.com>
2018-07-12 23:25:13 +0800

    1065

--- 1065.cpp
+++ 1065.cpp
@@ -8,7 +8,7 @@ typedef long long ll;
 typedef pair<int, int> PII;
 const int maxn = 5000 + 5;
 
-int n, vis[maxn];
+int n, vis[maxn], dp[maxn];
 PII p[maxn];
 
 int main(){
@@ -19,17 +19,26 @@ int main(){
             cin >> p[i].first >> p[i].second;
         }
         sort(p, p + n); ms(vis, 0);
-        int res = 0;
-        for (int i = 0; i < n; i++){
-            if (vis[i]) continue;
-            int x = p[i].second;
-            for (int j = i; j < n; j++){
-                if (p[j].second >= x && !vis[j]){
-                    vis[j] = 1;
-                    x = p[j].second;
-                }
+        int res = 1;
+        // for (int i = 0; i < n; i++){
+        //     if (vis[i]) continue;
+        //     int x = p[i].second;
+        //     for (int j = i; j < n; j++){
+        //         if (p[j].second >= x && !vis[j]){
+        //             vis[j] = 1;
+        //             x = p[j].second;
+        //         }
+        //     }
+        //     res++;
+        // }
+        reverse(p, p + n);
+        dp[1] = p[0].second;
+        for (int i = 1; i < n; i++){
+            if (p[i].second > dp[res]) dp[++res] = p[i].second;
+            else {
+                int k = lower_bound(dp + 1, dp + 1 + res, p[i].second) - dp;
+                dp[k] = p[i].second;
             }
-            res++;
         }
         cout << res << endl;
     }
