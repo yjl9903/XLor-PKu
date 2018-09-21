4ad7797473e36490b7e2dcd0727cfea3153f197f
X1 Carbon <yjl9903@vip.qq.com>
2018-09-21 16:03:48 +0800

    1182

--- 1182.cpp
+++ 1182.cpp
@@ -3,43 +3,40 @@
 #include <algorithm>
 #define ms(a,b) memset(a,b,sizeof(a))
 using namespace std;
-const int maxn = 50000 + 5;
+const int maxn = 100000 + 5;
 
-int pre[maxn], rank[maxn], N, K, res = 0;
-void init(){
-    for (int i = 0; i < maxn; i++){
-        pre[i] = i;
-        rank[i] = 0;
+int pre[maxn], val[maxn], n, k, ans;
+void init(){ for (int i = 0; i < maxn; i++) pre[i] = i, val[i] = 0; }
+int find(int x){ 
+    if (x == pre[x]) return x;
+    int t = find(pre[x]);
+    val[x] = (val[x] + val[pre[x]]) % 3;
+    return pre[x] = t;
+}
+bool join(int x, int y, int t){ 
+    int xx = find(x), yy = find(y);
+    if (xx == yy){
+        return (val[x] - val[y] + 3) % 3 == t;
     }
-}
-int find(int x){
-    return x == pre[x] ? x : pre[x] = find(pre[x]);
-}
-void join(int x, int y){
-    x = find(x); y = find(y);
-    pre[y] = x;
+    pre[xx] = yy;
+    val[xx] = (val[y] - val[x] + t + 6) % 3;
+    return true;
 }
 
 int main(){
-    init(); int cmd, x, y;
-    scanf("%d%d", &N, &K);
-    while (K--){
-        scanf("%d%d%d", &cmd, &x, &y);
-        if (x > N || y > N){
-            res++;
-            continue;
+    scanf("%d%d", &n, &k); init();
+    int op, x, y;
+    while (k--){
+        scanf("%d%d%d", &op, &x, &y);
+        if (x > n || y > n) {ans++; continue;}
+        if (op == 1){
+            if (!join(x, y, 0)) ans++;
         }
-        if (cmd == 1){
-
-        }
-        if (cmd == 2){
-            if (x == y){
-                res++;
-                continue;
-            }
-
+        else if (op == 2){
+            if (x == y) {ans++; continue;}
+            if (!join(x, y, 1)) ans++;
         }
     }
-    printf("%d", res);
+    printf("%d", ans);
     return 0;
 }
